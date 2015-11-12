21b3c9c037e98174c83c407cdeb9e76eeb384582
Hok Shun Poon <hokshunpoon@hoks-mbp.lan>
2015-11-12 14:00:46 +0000

    Add project files.

---
+++ Project2/Project2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Project2
+//
+//  Created by Hok Shun Poon on 12/11/2015.
+//  Copyright © 2015 Hok Shun Poon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Project2/Project2Tests/Project2Tests.m
@@ -0,0 +1,39 @@
+//
+//  Project2Tests.m
+//  Project2Tests
+//
+//  Created by Hok Shun Poon on 12/11/2015.
+//  Copyright © 2015 Hok Shun Poon. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Project2Tests : XCTestCase
+
+@end
+
+@implementation Project2Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
