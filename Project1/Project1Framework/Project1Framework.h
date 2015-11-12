5d3ab516af41c419080ef6fe5098bfa6818cb827
Hok Shun Poon <hokshunpoon@hoks-mbp.lan>
2015-11-12 14:21:22 +0000

    Update the framework header.

---
+++ Project1/Project1Framework/Project1Framework.h
@@ -0,0 +1,20 @@
+//
+//  Project1Framework.h
+//  Project1Framework
+//
+//  Created by Hok Shun Poon on 12/11/2015.
+//  Copyright © 2015 Hok Shun Poon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Project1Framework.
+FOUNDATION_EXPORT double Project1FrameworkVersionNumber;
+
+//! Project version string for Project1Framework.
+FOUNDATION_EXPORT const unsigned char Project1FrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like
+#import <Project1Framework/StringFunctions.h>
+
+
