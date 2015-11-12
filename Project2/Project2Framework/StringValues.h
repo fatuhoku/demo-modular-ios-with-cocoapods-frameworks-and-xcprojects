b604b3e46a1d8cb1522adf57cd74f2b802973dc4
Hok Shun Poon <hokshunpoon@hoks-mbp.lan>
2015-11-12 14:30:53 +0000

    Add new StringValues class.

---
+++ Project2/Project2Framework/StringValues.h
@@ -0,0 +1,13 @@
+//
+//  StringValues.h
+//  Project2
+//
+//  Created by Hok Shun Poon on 12/11/2015.
+//  Copyright © 2015 Hok Shun Poon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface StringValues : NSObject
++ (NSAttributedString *)helloWorldValue;
+@end
---
+++ Project2/Project2Framework/StringValues.m
@@ -0,0 +1,17 @@
+//
+//  StringValues.m
+//  Project2
+//
+//  Created by Hok Shun Poon on 12/11/2015.
+//  Copyright © 2015 Hok Shun Poon. All rights reserved.
+//
+
+#import "StringValues.h"
+
+@implementation StringValues
+
++ (NSAttributedString *)helloWorldValue {
+    return [NSAttributedString new];
+}
+
+@end
